46fa948e3ea0821a9340ef088b2aeb15170252e1
Nishant Bhandari <nishant.bhandari@unifize.com>
2025-05-14 18:05:59 +0530

    Added build formats & setup the package for publishing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig(({ command }) => {
   const isDev = command === "serve";
 
   return {
     root: isDev ? "examples" : ".",
-    plugins: [react()],
+    plugins: [
+      react(),
+      !isDev &&
+        dts({
+          insertTypesEntry: true,
+          outDir: "dist/types",
+        }),
+    ],
     resolve: {
       alias: {
         "@src": path.resolve(__dirname, "src"),
@@ -19,6 +27,7 @@ export default defineConfig(({ command }) => {
         entry: path.resolve(__dirname, "src/AutoSizeInput.tsx"),
         name: "AutoSizeInputReact",
         fileName: (format) => `autosize-input-react.${format}.js`,
+        formats: ["es", "cjs", "umd"],
       },
       rollupOptions: {
         external: ["react", "react-dom"],
